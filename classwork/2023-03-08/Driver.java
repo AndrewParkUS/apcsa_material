284369cfcf2e4e86b257ef9d8d1db5abf1a36c96
Andrew Park <102393936+AndrewParkUS@users.noreply.github.com>
2023-03-09 08:39:17 -0500

    Add files via upload

--- classwork/2023-03-08/Driver.java
+++ classwork/2023-03-08/Driver.java
@@ -5,8 +5,26 @@ public class Driver {
   public static void main(String[] args) {
     MyLinkedList myList = new MyLinkedList();
     
-    myList.add("test");
-    System.out.println(myList);
+    // 1. Testing add (at front)
+    myList.add("banana");
+    System.out.println("Testing Add: " + myList); // Testing toString implicitly
+
+    // 2. Testing get
+    System.out.println("Testing Get: " + myList.get(0));
+
+    // 3. Testing size
+    System.out.println("Testing Size: " + myList.size());
+
+    // 4. Testing add (at index)
+    myList.add(1, "apple");
+    System.out.println("Testing Add at Index: " + myList);
+
+    // 5. Testing indexOf
+    System.out.println("Testing indexOf: " + myList.indexOf("banana"));
+
+    // 6. Testing remove
+    myList.remove(1);
+    System.out.println("Testing Remove: " + myList);
 
   }
 }
--- classwork/2023-03-08/MyLinkedList.java
+++ classwork/2023-03-08/MyLinkedList.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-
 public class MyLinkedList {
 
   private Node head;
@@ -10,67 +9,98 @@ public class MyLinkedList {
     head = null;
   }
 
-
   /* Add a new node which data value to the front of the list */
-  public void add(String value) {
-    Node tempNode = new Node(value);
-    tempNode.next = head;
-    head = tempNode;
-
+  public void add (String value) {
+    Node temp = new Node(value);
+    temp.setNext(head);
+    head = temp;
   }
 
   /* Returns the value in the node at location index. */
   public String get(int index) {
-    String value = "";
-  
     // Create a reference to head. It will help loop through the list
     Node walker = head;
     // Counter to track a node position
     int counter = 0;
-    
-    return value;
+
+    while (walker != null && counter != index) {
+      walker = walker.getNext();
+      counter++;
+    }
+
+    return walker.getData();
   }
 
   /* Return the list as a string */
-  public String toString() {
-    String stringified = "";
+  public String toString() { // Overwriting built-in toString
     // Create a reference to head. It will help loop through the list
     Node walker = head;
-    
-    return stringified;
+    String list = "";
+
+    while (walker != null) {
+      list += walker.toString();
+      walker = walker.getNext();
+    }
+
+    list += "null";
+    return list;
   }
 
   /* Returns the number of elements in the list */
   public int size() {
-    int size = 0;
-    Node temp = head;
+    int len = 0;
+    Node walker = head;
 
-    while (temp != null) {
-      temp = temp.next;
-      size++;
+    while (walker != null) {
+      walker = walker.getNext();
+      len++;
     }
 
-    return size;
-
+    return len;
   }
 
 
   /*Adds a new node which contains value at index */
-  public void add(int index, String value) {
+  public void add(int index, String value){
+    Node temp = new Node(value); // Similar to add at front method
+    Node walker = head;
+    int counter = 0;
 
+    while (counter != index - 1) {
+      walker = walker.getNext();
+      counter++;
+    }
+
+    temp.setNext(walker.getNext());
+    walker.setNext(temp);
   }
 
 
   /* Returns the index of the first node in the list that contains value. */
   public int indexOf(String value) {
     int index = 0;
-    // insert
+    Node walker = head;
+
+    while (walker != null && !walker.getData().equals(value)) { // While walker isn't null and walker data DOESN'T equal value...
+      walker = walker.getNext();
+      index++;
+    }
+
     return index;
   }
 
 
-  /* Remove node al location index */
-
+  /* Remove node at location index */
   public void remove(int index) {
+    Node prev = null;
+    Node current = head;
+    
+    for (int i = 0; i < index; i++) {
+        prev = current;
+        current = current.getNext();
+    }
+
+    prev.setNext(current.getNext());
   }
 }
+
