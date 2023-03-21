f1acd2d64102c67d1ba88ca995689834bcc1436b
Andrew Park <102393936+AndrewParkUS@users.noreply.github.com>
2023-03-21 08:39:24 -0400

    Update MergeSort.java

--- classwork/MergeSort/MergeSort.java
+++ classwork/MergeSort/MergeSort.java
@@ -1,7 +1,15 @@
 public class MergeSort {
+  public static void MergeSort (int[] destination, int[] left, int[] right) {
+    if (left.length + right.length == destination.length) {
+        merge(destination, left, right);
+    } else {
+      // Unfinished
+    }
+    
+  }
+  
   public static int[] merge(int[] left, int[] right) {
-    // NOT FINISHED
-    // returnarray will be returned, leftindex indexing "through" left, rightindex indexing "through" right, allindex is "through" both
+    // returnarray will be returned, leftindex indexes "through" left, rightindex indexes "through" right, allindex goes "through" both
     int[] returnarray = new int[left.length + right.length];
     int leftindex = 0;
     int rightindex = 0;
@@ -11,7 +19,7 @@ public class MergeSort {
     while (leftindex < left.length && j < right.length) {
       
       if (left[leftindex] <= right[rightindex]) { 
-        // If left's element is less than right (or it's equal)
+        // If left's element is less than right (or equal)
         returnarray[allindex] = left[leftindex];
         leftindex++; // Only increment i or j to keep comparing same element w/ other element
         allindex++;
@@ -24,9 +32,19 @@ public class MergeSort {
       
     }
     
-    // Still need to deal with the remaining elements that weren't added
-    // More while loops? For loop?
+    // Looping through the remaining elements that weren't added
+    while (leftindex < left.length) { // For left list
+        returnarray[allindex] = left[leftindex];
+        leftindex++;
+        allindex++;
+    }
+
+    while (rightindex < right.length) { // Same idea as above but for right list
+        returnarray[allindex] = right[rightindex];
+        rightindex++;
+        allindex++;
+    }
     
-    return returnarray;
+    return returnarray; // Return merged array
   }
 }
