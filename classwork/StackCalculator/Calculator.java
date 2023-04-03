c1379f96c4380e224d18d6157c77b22f55f329a7
Andrew Park <102393936+AndrewParkUS@users.noreply.github.com>
2023-04-03 08:38:34 -0400

    Create Calculator.java

---
+++ classwork/StackCalculator/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+  
+  // Evaluate a postfix expression stored in expression
+  public static double eval (String expression) {
+    int returnvalue = 0;
+    int operand1 = expression.substring[0];
+    int operand2 = expression.substring[1];
+    String operator = expression.substring[expression.length-1];
+    for (int i = 0; i < expression.length-1; i++) {
+       expression.substring[i]
+    }
+    // if statements to figure out which operator to use
+    return returnvalue; // placeholder, operator should be different
+  }
+}
