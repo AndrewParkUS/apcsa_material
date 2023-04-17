9e4106b51b9a06d8cf30c82df07febb0c20fd7f3
Andrew Park <102393936+AndrewParkUS@users.noreply.github.com>
2023-04-17 08:41:18 -0400

    Create Human.java

---
+++ classwork/Inheritance/Human.java
@@ -0,0 +1,18 @@
+public class Human {
+  private bool deadOrNot;
+  
+  public Human() {
+   deadOrNot = false;
+  }
+}
+
+public class German extends Human {
+  private int age;
+  public German(int height) {
+    super();
+  }
+  
+public class Beethoven extends German {
+  private bool true;
+  public Beethoven(int
+}
